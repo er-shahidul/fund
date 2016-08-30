168928029a5d64c720881d365a9e4817d319265c
rahat <rahat.bubt@gmail.com>
2016-08-30 11:51:26 +0600

    organization campaign image  resizing done

--- src/Bundle/AppBundle/Resources/public/js/facebook/fbCustom.js
+++ src/Bundle/AppBundle/Resources/public/js/facebook/fbCustom.js
@@ -22,11 +22,13 @@
      FB.init({
          appId: facebookClientId, status: true, cookie: true, xfbml: true });
  };
+
  (function(d, debug){var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
      if(d.getElementById(id)) {return;}
      js = d.createElement('script'); js.id = id;
      js.async = true;js.src = "//connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
      ref.parentNode.insertBefore(js, ref);}(document, /*debug*/ false));
+
  function postToFeed(title, desc, url, image){
      var obj = {method: 'feed',link: url, picture: 'http://www.url.com/images/'+image,name: title,description: desc};
      function callback(response){}
